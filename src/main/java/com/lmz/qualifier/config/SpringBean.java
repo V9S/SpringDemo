c59bc6eff99a2231400ee13db693be8a4d0c28b9
zhenglianmin <zhenglianmin@jiuqi.com.cn>
2020-05-07 15:58:20 +0800

    update

--- src/main/java/com/lmz/qualifier/config/SpringBean.java
+++ src/main/java/com/lmz/qualifier/config/SpringBean.java
@@ -1,8 +1,25 @@
 package com.lmz.qualifier.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.lmz.qualifier.service.InterfacePrint;
+import com.lmz.qualifier.service.impl.PrintTest1;
+import com.lmz.qualifier.service.impl.PrintTest2;
+
+/**
+ * @author ZLM
+ *
+ */
 @Configuration
 public class SpringBean {
+	@Bean(value = "1")
+	public InterfacePrint test1() {
+		return new PrintTest1();
+	}
 
+	@Bean(value = "2")
+	public InterfacePrint test2() {
+		return new PrintTest2();
+	}
 }
--- src/main/java/com/lmz/qualifier/web/Controller.java
+++ src/main/java/com/lmz/qualifier/web/Controller.java
@@ -1,12 +1,31 @@
 package com.lmz.qualifier.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lmz.qualifier.service.InterfacePrint;
+
+/**
+ * @author ZLM
+ *
+ */
 @RestController
 public class Controller {
-	
-	public String print() {
-		
-		return "";
+
+	@Autowired
+	@Qualifier(value = "1")
+	private InterfacePrint i;
+
+	@RequestMapping("/print")
+	private String print() {
+		return random();
+	}
+
+	private String random() {
+
+		return String.valueOf(Math.round(Math.random() * 10) % 2);
+
 	}
 }
